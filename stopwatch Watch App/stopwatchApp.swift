78f42a768e2be6e0468f3b1e02b1f2158fbf371e
shamrat1 <yshamrat@gmail.com>
2023-09-25 17:33:52 +0600

    Initial Commit

---
+++ stopwatch Watch App/stopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  stopwatchApp.swift
+//  stopwatch Watch App
+//
+//  Created by Marsa Technologies on 25/9/23.
+//
+
+import SwiftUI
+
+@main
+struct stopwatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
